c6e80fe92562508f8f7e155ab19d495f1e2c33bb
Renan Bernordi <altendorfme@gmail.com>
2024-10-30 19:17:37 -0300

    remove arquivo de configuracao legado

--- src/data/config.default.php
+++
@@ -1,7 +0,0 @@
-<?php
-const TITLE = 'oPodSync';
-const BASE_URL = 'https://opodsync.test/';
-const ENABLE_SUBSCRIPTIONS = true;
-const ADMIN_PASSWORD = 'PASSWORD';
-const DEBUG = __DIR__ . '/debug.log';
-const DISABLE_USER_METADATA_UPDATE = true; 
