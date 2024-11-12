afe5c6d4350037f72826cc9e6f88c23e31e188de
Renan Bernordi <altendorfme@gmail.com>
2024-11-11 23:55:12 -0300

    ajuste de espaçamento no footer

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -5,7 +5,7 @@ function html_foot() { ?>
 
 			<footer class="rounded-3 pt-4 pt-md-3 pb-4 pb-md-3 ps-4 pe-4 bg-secondary-subtle text-center">
 				<p class="m-0"><?php echo __('footer.managed_by'); ?> <a class="link-secondary " href="https://pcdomanual.com/" target="_blank">PC do Manual</a> <?php echo __('actions.on'); ?> <a class="link-secondary " href="https://manualdousuario.net" target="_blank">Manual do Usuario</a>.</p>
-				<p class="m-0"><?php echo __('footer.with_love_by'); ?> <a class="link-secondary " href="https://altendorfme.com/" target="_blank">altendorfme</a><?php echo __('footer.version'); ?>: <?php echo VERSION; ?></p>
+				<p class="m-0"><?php echo __('footer.with_love_by'); ?> <a class="link-secondary " href="https://altendorfme.com/" target="_blank">altendorfme</a> - <?php echo __('footer.version'); ?>: <?php echo VERSION; ?></p>
 			</footer>
 		</div>
 		<script src="//cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
