a5b763131ec4cd4fc9eddfa31ae8e391ee6d0955
Renan Bernordi <altendorfme@gmail.com>
2024-11-17 12:54:54 -0300

    error log revert

--- src/inc/Errors.php
+++ src/inc/Errors.php
@@ -16,6 +16,8 @@ set_error_handler(static function ($severity, $message, $file, $line) {
 	throw new \ErrorException($message, 0, $severity, $file, $line);
 });
 
+ini_set('error_log', __DIR__ . '/../logs/error.log');
+
 set_exception_handler(function ($e) {
 	@http_response_code(500);
 	error_log((string)$e);
@@ -24,7 +26,7 @@ set_exception_handler(function ($e) {
 	
 	error_log((string) $e);
 
-	if (defined('DEBUG') && DEBUG == true) {
+	if (defined('DEBUG') && DEBUG) {
 		echo $e;
 
 		global $backtrace;
